3c1eac2edb2bf2dc0de8d049fe670b5278750d31
italo <italo@Rafaels-iMac.local>
2018-06-29 14:20:52 -0300

    conversor

---
+++ carteira_br_USA.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+dindin = float(input('Digite quantos reais tem em sua carteira: '))
+dolar = dindin / 3.85
+euro = dindin /  4.50
+print('Você contém {}R$, em dolares Você teria {}$, e em euros {}€.'.format(dindin, dolar, euro))
