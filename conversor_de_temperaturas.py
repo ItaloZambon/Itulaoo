1f7b5277351a2432744d3995bd5619137a588352
italo <italo@Rafaels-iMac.local>
2018-06-29 16:54:46 -0300

    conversor de temperatura

---
+++ conversor_de_temperaturas.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+c = float(input('Insira a temperatura em ºC: '))
+f = c * 1.8 + 32
+k = c + 273.15
+print( 'A sua temperatura em Celcius é {}, em Kelvin {}, e em Farenheit é {}'.format(c, k, f))
