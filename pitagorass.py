fea50ba1e82258b6683ee3b51a461bad3c7a8683
italo <italo@Rafaels-iMac.local>
2018-07-02 13:22:12 -0300

    Calculo_de_Pitagoras_

---
+++ pitagorass.py
@@ -0,0 +1,4 @@
+cato = float(input('Comprimento do cateto oposto: '))
+cata = float(input('Comprimento do cateto adjacente: '))
+hip = (cato ** 2 + cata ** 2) ** 0.5
+print('A Hipotenusa vai medir {}'.format(hip))
